31945e7ef62ceb4682053105dfb51f90e94dad8c
Mike George <mike@tallduck.com>
2016-06-30 17:18:46 -0400

    add visual feedback to form error and success

--- app/routes/auth/register.js
+++ app/routes/auth/register.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
 
 const {
-  Route
+  Route,
+  inject
 } = Ember;
 
 export default Route.extend({
+  flashMessages: inject.service(),
+
   model() {
     return this.store.createRecord('user');
   },
@@ -13,6 +16,10 @@ export default Route.extend({
     doRegister() {
       this.get('currentModel').save().then(() => {
         this.transitionTo('auth.login');
+        this.get('flashMessages').success('Registered! Please login now.');
+      }).catch((response) => {
+        const { errors } = response;
+        this.get('flashMessages').danger(errors.mapBy('detail').join(', '));
       });
     }
   }
